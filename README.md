cabf1156aaa5405ca1a4dffebb4a641516755b22
darklorian <lolpokens@mail.ru>
2023-08-15 02:53:22 +0400

    Add docker-compose, poetry and their dependencies

---
+++ README.md
@@ -0,0 +1,24 @@
+# VixenFood
+### About
+Сервис, реализующий механику сайта по предоставлению\созданию рецептов для всех! (без регистрации).
+# Built with
+![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white)
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+![Django](https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white)
+
+![Postgres](https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white)
+
+# RoadMap
+- [x] Начать проект
+- [x] Создать базовый функционал приложения 
+- [x] Обновить базовый функционал приложения 
+- [] Добавить сортировку, фильтры
+- [] Запустить докер-компоуз
+- [] Причесать код
+- [] Написать тесты
+- [] Написать документацию
+
+# Contacts
+🦊 Grand developer - [@darklorianprime](https://vk.com/darklorianprime) - kasimov.alexander.ul@gmail.com
--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -9,18 +9,16 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
+from os import getenv
 from pathlib import Path
-from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("django_secret_key")
+SECRET_KEY = getenv("SECURITY_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,10 +77,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("database"),
-        "USER": os.getenv("user"),
-        "PASSWORD": os.getenv("password"),
-        "HOST": os.getenv("host")
+        'NAME': getenv("POSTGRES_DATABASE"),
+        "USER": getenv("POSTGRES_USER"),
+        "PASSWORD": getenv("POSTGRES_PASSWORD"),
+        "HOST": getenv("POSTGRES_HOST")
     }
 }
 
